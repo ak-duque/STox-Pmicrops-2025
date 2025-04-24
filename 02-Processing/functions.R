085258823a0bb67214d5ba826ad077d2819401f4
Andrea Duque <duque.andrea2000@gmail.com>
2025-04-24 22:43:36 +0100

    Add select_best_hit and save_to_excel functions

--- 02-Processing/functions.R
+++ 02-Processing/functions.R
@@ -19,7 +19,7 @@
 # package dependencies
 
 ## ???
-
+library(openxlsx)
 
 ## BiocManager
 library(tximport) 
@@ -76,6 +76,7 @@ load_blastp_data <- function(filename){
 
 
 
+
 #' Add "GeneID" column to Blastp data
 #' 
 #' Add's "GeneID" column to Blastp data by splitting "qseqid" at "i"
@@ -94,14 +95,63 @@ Add_GeneID_column <- function(blast_data){
 
 
 
+#' Select the best hits in Blast
+#' 
+#' Explicar o que é que acontece aqui!!!
+#' 
+#' @param blast_data data frame with Blastp results (must include "GeneID" column)
+#' @return 
+#'  
+select_best_hits <- function(blast_data){
+  
+  # Add GeneID column 
+  blast_data <- Add_GeneID_column(blast_data)
+  
+  # Order: lowest evalue, highest pident
+  blast_data <- blast_data[order(blast_data$evalue, -blast_data$pident, decreasing = TRUE),]
+  
+  # Best hits (first row)
+  best_hits <- blast_data[match(unique(blast_data$GeneID), blast_data$GeneID), ]
+  
+  return(best_hits)
+  
+}
 
 
 
 
 
-
-
-
+#' Save a dataframe or a list of dataframes to an Excel file
+#' 
+#' @param data A dataframe or a named list of dataframes
+#' @param output_file The filename for the Excel file
+#' @return Saves the data to an Excel file
+#' 
+#' @example 
+#' save_to_excel(data, "/03-Output/01-DEG-Analysis/fileName.xlsx")
+#' save_to_excel(data, "fileName.xlsx")
+#' 
+save_to_excel <- function(data, output_file) {
+  
+  work_book <- createWorkbook()
+  
+  if (is.data.frame(data)) {
+    
+    sheet_name <- deparse(substitute(data))  # Get the variable name
+    addWorksheet(work_book, sheet_name)
+    writeData(work_book, sheet_name, data)
+    
+  } else if (is.list(data) && all(sapply(data, is.data.frame))) {
+    
+    for (name in names(data)) {
+      addWorksheet(work_book, name)
+      writeData(work_book, name, data[[name]])
+    }
+  }
+  
+  saveWorkbook(work_book, file = output_file, overwrite = TRUE)
+  cat("Excel created successfully:", output_file, "\n")
+}
 
 
 
