dca4e6b492c22fecf5195a882f3bdfbdabeab02f
Andrea Duque <duque.andrea2000@gmail.com>
2025-05-06 17:26:29 +0100

    Change ouput directorys Add perform DEG analysis (top10, degs) Prepared data to barplot

--- 02-Processing/main.R
+++ 02-Processing/main.R
@@ -113,7 +113,7 @@ View(head(abundance_tsv$abundance)) # matrix containing TPM's
 View(head(abundance_tsv$counts)) # matrix containing read counts
 View(head(abundance_tsv$length))
 
-save(abundance_tsv, file = "03-Output/01-DEG-Analysis/processed-data/abundance_tsv")
+save(abundance_tsv, file = "03-Output/01-DEG-Analysis/analysis-ready-data/abundance_tsv")
 
 
 
@@ -156,9 +156,9 @@ dim(BpK_plat) # 3.743 34
 
 
 ## Save files
-save_to_excel(BpK_swissprot, "03-Output/01-DEG-Analysis/processed-data/BpK-Swissprot.xlsx")
-save_to_excel(BpK_zebrafish, "03-Output/01-DEG-Analysis/processed-data/BpK-Zebrafish.xlsx")
-save_to_excel(BpK_plat, "03-Output/01-DEG-Analysis/processed-data/BpK-Plat.xlsx")
+save_to_excel(BpK_swissprot, "03-Output/01-DEG-Analysis/preprocessed-data/BpK-Swissprot.xlsx")
+save_to_excel(BpK_zebrafish, "03-Output/01-DEG-Analysis/preprocessed-data/BpK-Zebrafish.xlsx")
+save_to_excel(BpK_plat, "03-Output/01-DEG-Analysis/preprocessed-data/BpK-Plat.xlsx")
 
 
 
@@ -168,17 +168,18 @@ save_to_excel(BpK_plat, "03-Output/01-DEG-Analysis/processed-data/BpK-Plat.xlsx"
 # ==============================================================================
 
 # Data needed:
-BpK_swissprot <- read.xlsx("03-Output/01-DEG-Analysis/processed-data/BpK-Swissprot.xlsx")
-BpK_zebrafish <- read.xlsx("03-Output/01-DEG-Analysis/processed-data/BpK-Zebrafish.xlsx")
-BpK_plat <- read.xlsx("03-Output/01-DEG-Analysis/processed-data/BpK-Plat.xlsx")
+BpK_swissprot <- read.xlsx("03-Output/01-DEG-Analysis/preprocessed-data/BpK-Swissprot.xlsx")
+BpK_zebrafish <- read.xlsx("03-Output/01-DEG-Analysis/preprocessed-data/BpK-Zebrafish.xlsx")
+BpK_plat <- read.xlsx("03-Output/01-DEG-Analysis/preprocessed-data/BpK-Plat.xlsx")
 
-load("03-Output/01-DEG-Analysis/processed-data/abundance_tsv")
+load("03-Output/01-DEG-Analysis/analysis-ready-data/abundance_tsv")
 
 
 
-# We need to see if our data is good to analyse, since we are dealing with non-model organisms
-# So, we will start by analyzing the merge results
 
+
+
+# Prepare data to plot --------------
 
 
 S <- BpK_swissprot %>%
@@ -219,12 +220,187 @@ print(summary)
 
 
 
+# Data prepared --------------
+
+
+
+
+Data_SP <- process_BpK_data(BpK_swissprot) #15947
+save_to_excel(Data_SP, "./03-Output/01-DEG-Analysis/analysis-ready-data/BpK-data-processed-SP.xlsx")
+
+Data_Z <- process_BpK_data(BpK_zebrafish) #2975
+save_to_excel(Data_Z, "./03-Output/01-DEG-Analysis/analysis-ready-data/BpK-data-processed-Z.xlsx")
+
+Data_P <- process_BpK_data(BpK_plat) #145
+save_to_excel(Data_P, "./03-Output/01-DEG-Analysis/analysis-ready-data/BpK-data-processed-P.xlsx")
+
+
+# Prepare data to plot --------------
+S2 <- Data_SP %>%
+  select(c("pident","evalue")) %>%
+  mutate(blast = "Swissprot") # create a new column named blast
+
+Z2 <- Data_Z %>%
+  select(c("pident","evalue")) %>%
+  mutate(blast = "Zebrafish")
+
+P2 <- Data_P %>%
+  select(c("pident","evalue")) %>%
+  mutate(blast = "Platyhelminthes")
+
+all_blast2 <- bind_rows(S2,Z2,P2)
+head(all_blast2)
+View(all_blast2)
+
+
+all_blast2 <- all_blast2 %>%
+  mutate(
+    category = case_when(
+      pident < 30 ~ "Bad",
+      pident >= 30 & pident <= 70 ~ "Good",
+      pident > 70 ~ "Excellent"
+    )
+  )
+
+head(all_blast2)
+View(all_blast2)
+
+print(table(all_blast2$category))
+
+
+summary2 <- all_blast2 %>%
+  group_by(blast, category) %>% # agrupar
+  summarise(count = n(), .groups = "drop") %>%  #.groups = "drop" serve para desagrupar
+  group_by(blast) %>%
+  mutate(percentage = count / sum(count) * 100)
+
+
+ggplot(summary2, aes(x = category, y = percentage, fill = blast)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(
+    title = "Percentagem das categorias pident por base de dados",
+    x = "Categoria",
+    y = "Percentagem (%)",
+    fill = "Base de dados"
+  ) +
+  scale_y_continuous(labels = scales::percent_format(scale = 1))
+
+
+ggplot(summary2, aes(x = category, y = percentage, fill = blast)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_text(aes(label = sprintf("%.1f%%", percentage)),
+            position = position_dodge(width = 0.9), vjust = -0.5) +
+  labs(
+    title = "Percentagem das categorias pident por base de dados",
+    x = "Categoria",
+    y = "Percentagem (%)",
+    fill = "Base de dados"
+  ) +
+  scale_y_continuous(labels = scales::percent_format(scale = 1))
+
+# Data prepared --------------
 
 
 
 
 
 
+# Organism part is also missing!
+# Add PCA's part!! Is missing! 
 
 
 
+
+
+# ==============================================================================
+# TASK 3 - DEG analysis
+# ==============================================================================
+# Perform DEG analysis for swiss-prot and zebrafish datasets (with) and (without) 
+# filterByExpr filter
+# - explain what is done
+# - explain what is done
+# - explain what is done
+# ------------------------------------------------------------------------------
+
+
+
+# --- DEG Analysis for swiss-prot ----------------------------------------------
+## -- filter by expression (with) ----------------------------------------------
+
+
+deg_SP_with_filter <-run_and_save_DEG(Data_SP,
+                                      "SP",
+                                      model_type = "QLF" ,
+                                      use_filter = TRUE,
+                                      use_lfc = FALSE)
+
+
+# Available results and contrasts
+names(deg_SP_with_filter) 
+names(deg_SP_with_filter$DEG_result) 
+names(deg_SP_with_filter$decideTest_result) 
+
+# Example: see specific contrast and columns
+deg_SP <- deg_SP_with_filter$DEG_result
+Sp.RAvsTR <- deg_SP[["Sp.RAvsTR"]] 
+View(Sp.RAvsTR)
+
+colnames(Sp.RAvsTR) 
+
+View(Sp.RAvsTR[, c("logFC", "FDR", "evalue", "pident")]) 
+
+Sp.RAvsTR <- Sp.RAvsTR %>%
+  # from lowest FDR to highest
+  arrange(FDR)  
+
+View(Sp.RAvsTR)
+
+
+# Get top 10 + summary
+top10_SP_with_filter <- get_top10_degs(deg_SP, use_filter = TRUE, "SP") 
+
+
+
+# --- DEG Analysis for swiss-prot ----------------------------------------------
+## -- filter by expression (without) -------------------------------------------
+
+deg_SP_without_filter <-run_and_save_DEG(Data_SP,
+                                      "SP",
+                                      model_type = "QLF",
+                                      use_filter = FALSE,
+                                      use_lfc = FALSE)
+
+# Get top 10 + summary
+top10_SP_without_filter <- get_top10_degs(deg_SP_without_filter$DEG_result, 
+                                       use_filter = FALSE, "SP") 
+
+
+
+# --- DEG Analysis for zebrafish -----------------------------------------------
+## -- filter by expression (with) ----------------------------------------------
+
+deg_Z_with_filter <-run_and_save_DEG(Data_Z,
+                                      "Z",
+                                      model_type = "QLF",
+                                      use_filter = TRUE,
+                                      use_lfc = FALSE)
+
+
+# Get top 10 + summary
+top10_Z_with_filter <- get_top10_degs(deg_Z_with_filter$DEG_result, 
+                                       use_filter = TRUE, "Z") 
+
+
+
+# --- DEG Analysis for zebrafish -----------------------------------------------
+## -- filter by expression (without) -------------------------------------------
+
+deg_Z_without_filter<-run_and_save_DEG(Data_Z,
+                                       "Z",
+                                       model_type = "QLF",
+                                       use_filter = FALSE,
+                                       use_lfc = FALSE)
+
+# Get top 10 + summary
+top10_Z_without_filter <- get_top10_degs(deg_Z_without_filter$DEG_result, 
+                                      use_filter = FALSE, "Z") 
